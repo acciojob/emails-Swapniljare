52a2c27a54c46a6b7242968e36d8b4e86becd556
Swapnil <swapniljare9601@gmail.com>
2023-02-18 23:03:17 +0530

    first try

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,7 +5,7 @@ public class Email {
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,12 +18,41 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (oldPassword.equals(this.password)) {
+            if (newPassword.length() >= 8 && isValid(newPassword)) {
+                this.password = newPassword;
+            }
+        }
+    }
+
+    private boolean isValid(String newPassword) {
+        boolean upperCase = false;
+        boolean lowerCase = false;
+        boolean digit = false;
+        boolean specialCharacter = false;
+
+        for (int i = 0; i < newPassword.length(); i++) {
+            char ch = newPassword.charAt(i);
+
+            if (Character.isUpperCase(ch))
+                upperCase = true;
+            if (Character.isLowerCase(ch))
+                lowerCase = true;
+            if (Character.isDigit(ch))
+                digit = true;
+            if (!Character.isDigit(ch) && !Character.isLetter(ch) && !Character.isWhitespace(ch))
+                specialCharacter = true;
+
+        }
+        if (upperCase == true && lowerCase == true && digit == true && specialCharacter == true)
+            return true;
+        return false;
     }
 }
